79ec112d87cd8af77abe86e8fdb0a2ef5257884b
Mozahidur Rahman <mr.rousnay@gmail.com>
2020-07-29 05:53:34 +0600

    added - bookblock - need to fixed bugs

--- functions.php
+++ functions.php
@@ -146,7 +146,23 @@ function cloud_proposal_scripts() {
 	wp_enqueue_style( 'cloud-proposal-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'cloud-proposal-style', 'rtl', 'replace' );
 
+	wp_enqueue_script( 'cloud-proposal-modernizr-custom-js', get_template_directory_uri() . '/js/modernizr.custom.js', array('jquery'), _S_VERSION, false );
+
+
+
 	wp_enqueue_script( 'cloud-proposal-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+
+
+	wp_enqueue_script( 'cloud-proposal-jquerypp-custom-js', get_template_directory_uri() . '/js/jquerypp.custom.js', array('jquery'), _S_VERSION, true );
+
+
+	wp_enqueue_script( 'cloud-proposal-jquery-bookblock-js', get_template_directory_uri() . '/js/jquery.bookblock.min.js', array('jquery'), _S_VERSION, true );
+
+
+	wp_enqueue_script( 'cloud-proposal-theme-settings-js', get_template_directory_uri() . '/js/theme-settings.js', array('jquery'), _S_VERSION, true );
+
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- single-proposals.php
+++ single-proposals.php
@@ -10,20 +10,29 @@
 get_header();
 ?>
 
-<main id="primary" class="site-main">
-
-<?php if( have_rows('slides') ): ?>
-    <?php while( have_rows('slides') ): the_row(); ?>
-      <div id="slide-<?php echo get_row_index(); ?>">
-			        	<p><?php the_sub_field('slide_content'); ?></p>
-			        	<a><?php the_sub_field('read_more_text'); ?></a>
-			        </div>
-    <?php endwhile; ?>
-<?php endif; ?>
+	<main id="primary" class="site-main">
 
 
-		</main><!-- #main -->
+	<div id="bb-bookblock" class="bb-bookblock">
+	<?php if( have_rows('slides') ): ?>
+	    <?php while( have_rows('slides') ): the_row(); ?>
+	      <div class="bb-item" id="slide-<?php echo get_row_index(); ?>">
+				        	<p><?php the_sub_field('slide_content'); ?></p>
+				        	<a><?php the_sub_field('read_more_text'); ?></a>
+				        </div>
+	    <?php endwhile; ?>
+	<?php endif; ?>
+	</div>
 
-	<?php
-	get_sidebar();
-	get_footer();
+<nav>
+						<a id="bb-nav-first" href="#" class="bb-custom-icon bb-custom-icon-first">First page</a>
+						<a id="bb-nav-prev" href="#" class="bb-custom-icon bb-custom-icon-arrow-left">Previous</a>
+						<a id="bb-nav-next" href="#" class="bb-custom-icon bb-custom-icon-arrow-right">Next</a>
+						<a id="bb-nav-last" href="#" class="bb-custom-icon bb-custom-icon-last">Last page</a>
+					</nav>
+					
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
