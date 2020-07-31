355e85dcd188b3cb71034c3aa8c601e8e3b74e45
Mozahidur Rahman <mr.rousnay@gmail.com>
2020-07-31 16:09:49 +0600

    modified - Removes some admin menus

--- includes/theme-settings.php
+++ includes/theme-settings.php
@@ -73,3 +73,38 @@ function user_profile_options() {
     </script>
   <?php
 }
+
+
+/***********************************
+ * Removes some admin menus
+************************************/
+function remove_menus_for_admin(){
+  remove_menu_page( 'edit.php' );                   //Posts
+  remove_menu_page( 'edit.php?post_type=page' );    //Pages
+  remove_menu_page( 'edit-comments.php' );          //Comments
+}
+
+if ( current_user_can( 'administrator' ) ){
+    add_action( 'admin_menu', 'remove_menus_for_admin' );
+}
+
+
+function remove_menus_for_editor(){
+
+  remove_menu_page( 'jetpack' );                    //Jetpack* 
+  remove_menu_page( 'edit.php' );                   //Posts
+  remove_menu_page( 'edit.php?post_type=page' );    //Pages
+  remove_menu_page( 'edit-comments.php' );          //Comments
+  remove_menu_page( 'index.php' );                  //Dashboard
+  remove_menu_page( 'tools.php' );                  //Tools
+  remove_menu_page( 'themes.php' );                 //Appearance
+  remove_menu_page( 'plugins.php' );                //Plugins
+  remove_menu_page( 'options-general.php' );        //Settings
+  remove_menu_page( 'upload.php' );                 //Media
+  // remove_menu_page( 'users.php' );                  //Users
+   
+}
+
+if ( !current_user_can( 'administrator' ) ){
+    add_action( 'admin_menu', 'remove_menus_for_editor' );
+}
